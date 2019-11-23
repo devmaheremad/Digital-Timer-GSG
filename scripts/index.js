aafcd781240275fa147fb65ec23b3fd29ff02d9e
Maher E. Elmashharawi <53684138+maheremad@users.noreply.github.com>
2019-11-23 15:58:50 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,10 @@ let resetButton = document.createElement("button");
 let msTensCounter = 0;
 let msHundredsCounter = 0;
 let secondOnesCounter = 0;
+let fieldsetForBtn = document.createElement("fieldset");
+let fieldsetForTimer = document.createElement("fieldset");
+fieldsetForBtn.style = "padding: 20px;margin: 20px auto;text-align: center;width: 300px"
+document.body.insertBefore(fieldsetForBtn, document.body.firstElementChild);
 
 document.querySelectorAll(".digit").forEach(item => {
 
@@ -26,10 +30,10 @@ resetButton.onclick = function() {
     );
 }
 
-document.body.appendChild(btn);
+fieldsetForBtn.appendChild(btn);
 btn.textContent = "Start The Timer";
 
-document.body.appendChild(resetButton);
+fieldsetForBtn.appendChild(resetButton);
 resetButton.textContent = "Reset";
 btn.className = "btn";
 
@@ -72,3 +76,13 @@ btn.onclick = function timer(){
     }, 1000);
 
 };
+
+document.querySelectorAll("button").forEach(item => item.style = "box-shadow:inset 0px 0px 11px 2px #bbdaf7;background-color:#15589b;border-radius:50px 0 50px 0;border:2px solid #84bbf3;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;font-weight:bold;font-style:italic;padding:9px 24px;text-decoration:none;outline: none");
+
+document.querySelectorAll("button").forEach(item => item.onmouseover = function() {
+    this.style = "background:linear-gradient(to bottom, #378de5 5%, #79bbff 100%);background-color:#378de5;box-shadow:inset 0px 0px 11px 2px #bbdaf7;background-color:#15589b;border-radius:50px 0 50px 0;border:2px solid #84bbf3;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;font-weight:bold;font-style:italic;padding:9px 24px;text-decoration:none;outline: none";
+});
+
+document.querySelectorAll("button").forEach(item => item.onmouseleave = function() {
+    this.style = "box-shadow:inset 0px 0px 11px 2px #bbdaf7;background-color:#15589b;border-radius:50px 0 50px 0;border:2px solid #84bbf3;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;font-weight:bold;font-style:italic;padding:9px 24px;text-decoration:none;outline: none";
+});
