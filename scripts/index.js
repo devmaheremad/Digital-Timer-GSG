d86b09a4c5f5c272a43f1ded7ee500e16f1ca672
Maher E. Elmashharawi <53684138+maheremad@users.noreply.github.com>
2019-12-16 14:46:47 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,88 +1,114 @@
-let secondTens = document.getElementById('secondTens');
-let secondOnes = document.getElementById('secondOnes');
-let msHundreds = document.getElementById('msHundreds');
+let startBtn = document.createElement('button');
+let reset = document.createElement('button');
+let place = document.createElement('fieldset');
+
 let msTens = document.getElementById('msTens');
-let btn = document.createElement("button");
-let resetButton = document.createElement("button");
-let msTensCounter = 0;
-let msHundredsCounter = 0;
-let secondOnesCounter = 0;
-let fieldsetForBtn = document.createElement("fieldset");
-let fieldsetForTimer = document.createElement("fieldset");
-fieldsetForBtn.style = "padding: 20px;margin: 20px auto;text-align: center;width: 300px"
-document.body.insertBefore(fieldsetForBtn, document.body.firstElementChild);
+let msHundreds = document.getElementById('msHundreds');
+let secondOnes = document.getElementById('secondOnes');
+let secondTens = document.getElementById('secondTens');
 
-document.querySelectorAll(".digit").forEach(item => {
+let msTensTimer;
+let msHundredsTimer;
 
-    if(item.innerHTML === "-") {item.innerHTML = "0"}
-    }
+place.className = 'btncon';
 
-);
+document.body.insertBefore(place, document.body.firstElementChild);
 
-resetButton.onclick = function() {
+place.insertBefore(startBtn, place.firstElementChild);
+startBtn.textContent = 'Start Timer';
 
-    document.querySelectorAll(".digit").forEach(item => {
-        if(item.innerHTML != ":"){
-            item.textContent = "0";
-            document.getElementsByClassName("digits")[0].style.color = "#000";
-            }
-        }
-    );
-}
+place.insertBefore(reset, place.children[1]);
+reset.textContent = 'Reset All';
 
-fieldsetForBtn.appendChild(btn);
-btn.textContent = "Start The Timer";
+// This Function For Start Timer To 10S: 
+startBtn.onclick = function getTimer() {
 
-fieldsetForBtn.appendChild(resetButton);
-resetButton.textContent = "Reset";
-btn.className = "btn";
+    getMsTensTimer();
+    getMsHundredsTimer();
+    getSecondOnesTimer();
 
-btn.onclick = function timer(){
- 
-    let msTensTimer = setInterval(() => {
-        document.getElementsByClassName("btn")[0].disabled = true;
-        msTens.innerHTML = `${msTensCounter += 1}`;
-        if (msTens.innerHTML === '9') {
-            msTensCounter = 0;
-            msTens.innerHTML = 0;
-        }
+    startBtn.disabled = true;
+
+    document.querySelectorAll('.digit').forEach(item => item.style.color = 'black');
+
+    document.querySelectorAll('button')[0].style = 'color: rgb(27, 27, 27); border: 1px dotted white';
+
+};
+
+// This Function For Reset EveryThings To Zero Time: 
+reset.onclick = function getreset() {
+
+    msTens.textContent = '0';
+    msHundreds.textContent = '0';
+    secondOnes.textContent = '0';
+    secondTens.textContent = '0';
+    document.querySelectorAll('.digit').forEach(item => item.style.color = 'black');
+
+};
+
+// This Function For count 10ms:  
+function getMsTensTimer() {
+
+    msTens.textContent = '0';
+
+    msTensTimer = setInterval(() => {
+
+        msTens.textContent = (+msTens.textContent + 1);
+
+        if (msTens.textContent === '10') {
+
+            msTens.textContent = '0';
+
+        };
 
     }, 10);
 
-    let msHundredsTimer = setInterval(() => {
-        
-        msHundreds.innerHTML = `${msHundredsCounter += 1}`;
-        if (msHundreds.innerHTML === '9') {
-            msHundredsCounter = 0;
-            msHundreds.innerHTML = 0;
-        }
+};
+
+// This Function For count 100ms:  
+function getMsHundredsTimer() {
+
+    msHundreds.textContent = '0';
+
+    msHundredsTimer = setInterval(() => {
+
+        msHundreds.textContent = (+msHundreds.textContent + 1);
+
+        if (msHundreds.textContent === '10') {
+
+            msHundreds.textContent = '0';
+
+        };
 
     }, 100);
 
+};
+
+// This Function For count 1000ms:  
+function getSecondOnesTimer() {
+
+    secondOnes.textContent = '0';
+    secondTens.textContent = '0';
+
     let secondOnesTimer = setInterval(() => {
-        
-        secondOnes.innerHTML = `${secondOnesCounter += 1}`;
-        if (secondOnes.innerHTML === '9') {
-            secondOnes.innerHTML = '0';
-            secondOnesCounter = 0;
-            secondTens.innerHTML = 1;
+
+        secondOnes.textContent = (+secondOnes.textContent + 1);
+
+        if (secondOnes.textContent === '10') {
+
+            msTens.textContent = '0';
+            msHundreds.textContent = '0';
+            secondOnes.textContent = '0';
+            secondTens.textContent = '1';
             clearInterval(msTensTimer);
             clearInterval(msHundredsTimer);
             clearInterval(secondOnesTimer);
-            document.getElementsByClassName("digits")[0].style.color = "red";
-            document.getElementsByClassName("btn")[0].disabled = false;
-        }
+            document.querySelectorAll('.digit').forEach(item => item.style.color = 'red');
+            startBtn.disabled = false;
+            document.querySelectorAll('button')[0].style = 'color: white;';
+
+        };
 
     }, 1000);
 
 };
-
-document.querySelectorAll("button").forEach(item => item.style = "box-shadow:inset 0px 0px 11px 2px #bbdaf7;background-color:#15589b;border-radius:50px 0 50px 0;border:2px solid #84bbf3;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;font-weight:bold;font-style:italic;padding:9px 24px;text-decoration:none;outline: none");
-
-document.querySelectorAll("button").forEach(item => item.onmouseover = function() {
-    this.style = "background:linear-gradient(to bottom, #378de5 5%, #79bbff 100%);background-color:#378de5;box-shadow:inset 0px 0px 11px 2px #bbdaf7;background-color:#15589b;border-radius:50px 0 50px 0;border:2px solid #84bbf3;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;font-weight:bold;font-style:italic;padding:9px 24px;text-decoration:none;outline: none";
-});
-
-document.querySelectorAll("button").forEach(item => item.onmouseleave = function() {
-    this.style = "box-shadow:inset 0px 0px 11px 2px #bbdaf7;background-color:#15589b;border-radius:50px 0 50px 0;border:2px solid #84bbf3;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;font-weight:bold;font-style:italic;padding:9px 24px;text-decoration:none;outline: none";
-});
